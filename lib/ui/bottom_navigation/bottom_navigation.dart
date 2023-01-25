7bd8f14c239d01489fecfc1a71ef15daecf1167e
JavierGavra <javiergavra01@gmail.com>
2023-01-25 20:42:38 +0700

    setup project

---
+++ lib/ui/bottom_navigation/bottom_navigation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CustomBottomNavigation extends StatefulWidget {
+  const CustomBottomNavigation({super.key});
+
+  @override
+  State<CustomBottomNavigation> createState() => _CustomBottomNavigationState();
+}
+
+class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
